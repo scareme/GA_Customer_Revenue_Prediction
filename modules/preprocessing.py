f34ba0f664d1428fb7a104393bdd7b3656faec14
Kirill Ivanov <s.kirill.iv@gmail.com>
2018-11-12 22:12:02 +0300

    last

--- modules/preprocessing.py
+++ modules/preprocessing.py
@@ -1,5 +1,7 @@
 import json
 from zipfile import ZipFile
+from datetime import datetime
+
 import pandas as pd
 from pandas.io.json import json_normalize
 
@@ -27,3 +29,55 @@ def read_zip(zipfile, file, **kwards):
         with zip_file.open(file) as myfile:
             data = pd.read_csv(myfile, **kwards)
     return data
+
+
+def timestamp_to_str(df_column):
+    convert_dict = {
+        el: datetime.utcfromtimestamp(int(el))
+        for el in df_column.unique()
+    }
+    return df_column.map(convert_dict)
+
+
+def return_year(df_column):
+    convert_dict = {
+        el: datetime.utcfromtimestamp(int(el)).year
+        for el in df_column.unique()
+    }
+    return df_column.map(convert_dict)
+
+
+def return_month(df_column):
+    convert_dict = {
+        el: datetime.utcfromtimestamp(int(el)).month
+        for el in df_column.unique()
+    }
+    return df_column.map(convert_dict)
+
+
+def return_day(df_column):
+    convert_dict = {
+        el: datetime.utcfromtimestamp(int(el)).day
+        for el in df_column.unique()
+    }
+    return df_column.map(convert_dict)
+
+
+def return_hour(df_column):
+    convert_dict = {
+        el: datetime.utcfromtimestamp(int(el)).hour
+        for el in df_column.unique()
+    }
+    return df_column.map(convert_dict)
+
+
+def return_date(df_column):
+    convert_dict = {}
+    for key in df_column.unique():
+        year = str(datetime.utcfromtimestamp(int(key)).year)
+        month = datetime.utcfromtimestamp(int(key)).month
+        month = str(month) if month > 9 else '0' + str(month)
+        day = datetime.utcfromtimestamp(int(key)).day
+        day = str(day) if day > 9 else '0' + str(day)
+        convert_dict[key] = int(year + month + day)
+    return df_column.map(convert_dict)
